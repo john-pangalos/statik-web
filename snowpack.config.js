43a02af775f07d8c974120c079ecefc7029d35d1
John Pangalos <jpangs88@gmail.com>
2021-02-04 15:35:02 +0100

    Some work

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,29 +1,33 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    public: {url: '/', static: true},
-    src: {url: '/dist'},
+    public: { url: "/", static: true },
+    src: { url: "/dist" },
   },
   plugins: [
-    '@snowpack/plugin-svelte',
-    '@snowpack/plugin-dotenv',
-    '@snowpack/plugin-typescript',
+    "@snowpack/plugin-svelte",
+    "@snowpack/plugin-dotenv",
+    "@snowpack/plugin-typescript",
+    "@snowpack/plugin-postcss",
   ],
   routes: [
     /* Enable an SPA Fallback in development: */
     // {"match": "routes", "src": ".*", "dest": "/index.html"},
   ],
   optimize: {
-    /* Example: Bundle your final build: */
-    // "bundle": true,
+    splitting: true,
+    bundle: true,
+    minify: true,
+    treeshake: true,
+    target: "es2020",
   },
   packageOptions: {
     /* ... */
   },
   devOptions: {
-    /* ... */
+    sourcemap: true,
   },
   buildOptions: {
-    /* ... */
+    sourcemap: true,
   },
 };
